195b83bc29fc10fa80b571a93f9f036a28e89fab
mhockenhull <michaelhockenhull@gmail.com>
2016-12-05 10:02:58 +0100

    Complete initial setup of project, and commit files to git.

---
+++ fetch_data.py
@@ -0,0 +1,1 @@
+#script to fetch data from the web for the movie_markov_chain project
---
+++ run.py
@@ -0,0 +1,1 @@
+#This runs the script for the movie_markov_chain, the purpose of which it is to fetch script text and combine it in new ways. 
